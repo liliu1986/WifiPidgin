0b4eb1e6094a0674638cfdfac4778bcef36c1759
YeFeng.Wen <niuniu8866@gmail.com>
2015-04-03 23:30:37 -0400

    Implement the Channel and ChannelManager class
     * Channel Class:
        -Adding id as request by Database
        -Implement setter and getter method
     * ChannelManager:
        -channelMap is now implement using HashMap
        -Implement add, delete ,getChannelList and getChannelByIdentifier
     * Start coding on Chat class with simple constructor
    
     Changes to be committed:
    	modified:   app/src/main/java/com/iotbyte/wifipidgin/channel/Channel.java
    	modified:   app/src/main/java/com/iotbyte/wifipidgin/channel/ChannelManager.java
    	modified:   app/src/main/java/com/iotbyte/wifipidgin/chat/Chat.java

--- app/src/main/java/com/iotbyte/wifipidgin/channel/Channel.java
+++ app/src/main/java/com/iotbyte/wifipidgin/channel/Channel.java
@@ -3,6 +3,8 @@ package com.iotbyte.wifipidgin.channel;
 
 import android.util.Log;
 
+import com.iotbyte.wifipidgin.friend.Friend;
+
 import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -16,13 +18,15 @@ public class Channel {
     private String channelName;
     private String channelDescription;
     private String channelIdentifier;
-    private ArrayList<Friends> channelFriendsList;
+    private ArrayList<Friend> channelFriendList;
+    private long id;   // id is added as the primary key to be used in data persist
     public static final String CHANNEL_TAG = "Channel Class";
 
-    public Channel(ArrayList<Friends> friendsList, String channelName, String description) {
+    public Channel(ArrayList<Friend> friendList, String channelName, String description) {
         this.channelName = channelName;
         this.channelDescription = description;
-        this.channelFriendsList = friendsList;
+        this.channelFriendList = friendList;
+        this.id = -1;
 
         // The channelIdentifier is sha1 of creating device (wifi) MAC concatenate with channel
         // creation timestamp
@@ -33,7 +37,7 @@ public class Channel {
         //The MAC address feature will work on a real device but not on emulator
         //As the current emulator does not support WiFi
         String address = com.iotbyte.wifipidgin.utils.Utils.getMACAddress("wlan0");
-        Log.v(CHANNEL_TAG,"the MAC address is "+ address);
+        Log.v(CHANNEL_TAG, "the MAC address is " + address);
 
 
         // Get Timestamp
@@ -45,37 +49,71 @@ public class Channel {
         } catch (NoSuchAlgorithmException e) {
             this.channelIdentifier = identifier;
         }
-        Log.v(CHANNEL_TAG,"the channelIdentifier is "+ this.channelIdentifier);
+        Log.v(CHANNEL_TAG, "the channelIdentifier is " + this.channelIdentifier);
     }
 
+    /**
+     * Getter method of channelName
+     * @return channelName
+     */
     public String getChannelName() {
         return channelName;
     }
 
+    /**
+     * Setter method of channelName
+     * @param channelName
+     */
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    /**
+     * Getter method of channelDescription
+     * @return channelDescription
+     */
     public String getChannelDescription() {
         return channelDescription;
     }
 
+    /**
+     * Setter method of channelDescription
+     * @param channelDescription
+     */
+    public void setChannelDescription(String channelDescription) {
+        this.channelDescription = channelDescription;
+    }
+
+    /**
+     * Getter method of channelIdentifier
+     * @return channelIdentifier
+     */
     public String getChannelIdentifier() {
         return channelIdentifier;
     }
 
-    public ArrayList<Friends> getChannelFriendsList() {
-        return channelFriendsList;
+    /**
+     * Getter method of channelFriendsList
+     * @return channelFriendList
+     */
+    public ArrayList<Friend> getChannelFriendsList() {
+        return channelFriendList;
     }
 
-    //TODO: implement the getChannelStatus
-    public boolean getChannelStatus() {
-        return false;
+    /**
+     * Getter method of id
+     * @return id
+     */
+    public long getId() {
+        return id;
     }
 
+    /**
+     * Setter method of id
+     * @param id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
 }
 
-
-//TODO: Remove mock Friends Class
-
-class Friends {
-    Friends() {
-
-    }
-}
--- app/src/main/java/com/iotbyte/wifipidgin/channel/ChannelManager.java
+++ app/src/main/java/com/iotbyte/wifipidgin/channel/ChannelManager.java
@@ -2,6 +2,8 @@ package com.iotbyte.wifipidgin.channel;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by yefwen@iotbyte.com on 26/03/15.
@@ -9,22 +11,69 @@ import java.util.ArrayList;
 public class ChannelManager {
     private static ChannelManager instance = null;
 
-    private ArrayList<Channel> channelList ;
-    protected ChannelManager(){
+    private HashMap<String, Channel> channelMap; // ChannelIdentifier and Channel pair
+
+    protected ChannelManager() {
         //Calling database to grep existing channelList
         // or create a new channelList if there is nothing been retrieved
 
         //TODO: remove the mocked channelList
         //TODO: Adding channelList retrieval functionality from database
+        channelMap = new HashMap<String, Channel>();
     }
 
-    public static ChannelManager getInstance(){
-        if (instance == null){
+    public static ChannelManager getInstance() {
+        if (instance == null) {
             instance = new ChannelManager();
         }
         return instance;
     }
 
+    //TODO: implement the getChannelStatus
+    public boolean getChannelStatus(String channelIdentifier) {
+        return false;
+    }
+
+    /**
+     * Return the channel object with given channelIdentifier
+     * @param channelIdentifier the channelIdentifier of the channel under request
+     * @return Return the Channel with the specific channelIdentifier
+     */
+    public Channel getChannelByIdentifier(String channelIdentifier) {
+        if (channelMap.containsKey(channelIdentifier)) {
+            return channelMap.get(channelIdentifier);
+        } else
+            return null;
+    }
+
+    /**
+     * Add a channel to the ChannelManager
+     * @param channel the channel to be added
+     */
+    public void addChannel(Channel channel) {
+        channelMap.put(channel.getChannelIdentifier(), channel);
+        // NOTE: update the UI when this method is called
+    }
+
+    /**
+     * Remove a channel from ChannelManager
+     * @param channel the channel to be deleted
+     */
+    public void deleteChannel(Channel channel) {
+        channelMap.remove(channel.getChannelIdentifier());
+        // NOTE: update the UI when this method is called
+    }
+
+    /**
+     * Return a List representation of the channels managed under ChannelManager
+     * @return a List representation of the channels, which is backed by ArrayList
+     */
+    public List<Channel> getChannelList(){
+        //TODO: possible adding ordering due to latest change property of channel
+        List<Channel> result = new ArrayList<Channel>();
+        result.addAll(channelMap.values());
+        return result;
+    }
 
 
 }
