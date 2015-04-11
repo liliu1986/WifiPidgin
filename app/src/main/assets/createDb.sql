51bea2cdce26aa654a535bcca168be02c56ddc4a
Di Qiu <dqiu@iotbyte.com>
2015-04-11 02:16:10 -0400

    Fixed int PK not allowing autoincrement

--- app/src/main/assets/createDb.sql
+++ app/src/main/assets/createDb.sql
@@ -5,18 +5,18 @@
 
 CREATE TABLE friend
 (
-    _id         INT PRIMARY KEY AUTOINCREMENT,
+    _id         INTEGER PRIMARY KEY AUTOINCREMENT,
     mac_addr    TEXT NOT NULL UNIQUE,
-    ip          INT NOT NULL UNIQUE,
+    ip          INTEGER NOT NULL UNIQUE,
     name        TEXT,
     description TEXT,
-    status      INT,
-    is_favourite INT NOT NULL DEFAULT 0
+    status      INTEGER,
+    is_favourite INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE channel
 (
-    _id         INT PRIMARY KEY AUTOINCREMENT,
+    _id         INTEGER PRIMARY KEY AUTOINCREMENT,
     identifier  TEXT NOT NULL UNIQUE,
     name        TEXT,
     description TEXT
@@ -24,9 +24,9 @@ CREATE TABLE channel
 
 CREATE TABLE channel_friend_list
 (
-    _id         INT PRIMARY KEY AUTOINCREMENT,
-    friend_id   INT NOT NULL,
-    channel_id  INT NOT NULL,
+    _id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    friend_id   INTEGER NOT NULL,
+    channel_id  INTEGER NOT NULL,
     -- restrict on deletion of friend if it still belongs to a channel
     FOREIGN KEY(friend_id) REFERENCES friend(_id) ON DELETE RESTRICT,
     -- cascade delete all friends-channel record when channel is deleted
