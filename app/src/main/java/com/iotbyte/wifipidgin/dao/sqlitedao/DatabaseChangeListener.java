e89352684f6d76a11f98c73a2c49d90b47390220
Li Liu <liliueecg@gmail.com>
2015-05-11 23:57:27 -0400

    Delete DatabaseChangeListener.java

--- app/src/main/java/com/iotbyte/wifipidgin/dao/sqlitedao/DatabaseChangeListener.java
+++
@@ -1,8 +0,0 @@
-package com.iotbyte.wifipidgin.dao.sqlitedao;
-
-/**
- * Created by fire on 29/04/15.
- */
-public interface DatabaseChangeListener {
-    public void onMessageReceived(String msg);
-}
