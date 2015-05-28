4377a241d5fd007e5337cd3d1c99a47b9a5d2730
Li Liu <liliueecg@gmail.com>
2015-05-28 00:12:07 -0400

    Finished FriendCreationService

--- app/src/main/java/com/iotbyte/wifipidgin/nsdmodule/FriendCreationService.java
+++ app/src/main/java/com/iotbyte/wifipidgin/nsdmodule/FriendCreationService.java
@@ -31,19 +31,24 @@ public class FriendCreationService extends Service  {
 
         return Service.START_STICKY;
     }
+    @Override
+    public void onCreate() {
+    }
     class FriendCreationServiceThread implements Runnable {
 
         @Override
         public void run() {
             NsdClient nsdClientInstance = NsdClient.getInstance(getApplicationContext());
+            Log.d(TAG, "FriendCreationServiceThread Started");
 
             while (!Thread.currentThread().isInterrupted()) {
                 //Processing the queue
+                //Log.d(TAG, "FriendCreationServiceThread Started");
                 while (!nsdClientInstance.isEmptyFriendCreationQueue()){
                     try {
                         TimeUnit.MILLISECONDS.sleep(delayInterval);
                         Friend creatingFriend = nsdClientInstance.dequeueFriendCreationQueue();
-
+                        Log.d(TAG, "Creating friend " + creatingFriend.getMac().toString());
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
