30c1e0961757b85d7a27bba32aa98102f0d273e6
Li Liu <liliueecg@gmail.com>
2015-05-30 11:07:20 -0400

    Enabled file chooser to allow user select their image icon.

--- app/src/main/java/com/iotbyte/wifipidgin/ui/DisplayNamecardActivity.java
+++ app/src/main/java/com/iotbyte/wifipidgin/ui/DisplayNamecardActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -24,10 +25,12 @@ import com.iotbyte.wifipidgin.friend.Friend;
 import com.iotbyte.wifipidgin.utils.Utils;
 
 import java.io.File;
+import java.net.URISyntaxException;
 
 public class DisplayNamecardActivity extends ActionBarActivity implements View.OnClickListener {
 
     public final static String EXTRA_MESSAGE = "com.iotbyte.wifipidgin.NAMECARD";
+    public final static String EXTRA_MESSAGE_USERMAC = "com.iotbyte.wifipidgin.NAMECARD.USERMAC";
 
     FriendDao fd;
     String userName = "Default User Name";
@@ -38,8 +41,8 @@ public class DisplayNamecardActivity extends ActionBarActivity implements View.O
 
     private static final String TAG = "DisplayNameCard";
 
-
-
+    private String userMac = null;
+    private Friend inFriend = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -54,10 +57,10 @@ public class DisplayNamecardActivity extends ActionBarActivity implements View.O
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         //Get the user name from the intent
-        String userMac = intent.getStringExtra(EXTRA_MESSAGE);
+        userMac = intent.getStringExtra(EXTRA_MESSAGE);
         fd = DaoFactory.getInstance().getFriendDao(this.getApplicationContext(), DaoFactory.DaoType.SQLITE_DAO, null);
         Log.d(TAG, "Use MAC: "+ userMac);
-        Friend inFriend = fd.findByMacAddress(Utils.hexStringToByteArray(userMac));
+        inFriend = fd.findByMacAddress(Utils.hexStringToByteArray(userMac));
 
         if (inFriend != null) {
             userName = inFriend.getName();
@@ -68,6 +71,9 @@ public class DisplayNamecardActivity extends ActionBarActivity implements View.O
             //Set the user image
             File imgFile = new  File(inFriend.getImagePath());
             ImageView myImage = (ImageView) findViewById(R.id.user_image);
+            myImage.setOnClickListener(this);
+
+
             if(imgFile.exists()){
                 Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
                 myImage.setImageBitmap(myBitmap);
@@ -175,7 +181,65 @@ public class DisplayNamecardActivity extends ActionBarActivity implements View.O
 
                 //@TODO Start activity to chat
 
+            case R.id.user_image :
+                Log.d(TAG, "The path is " + getExternalFilesDir(null).toString());
+                showFileChooser();
+
         }
+    }
+
+    private static final int FILE_SELECT_CODE = 0;
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a image to represent yourself"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    Log.d(TAG, "File Uri: " + uri.toString());
+                    // Get the path
+                    String path = null;
+
+                    path = Utils.getRealPathFromURI(this, uri);
+                    Log.d(TAG, "File Path: " + path);
+                    //
+                    File imgFile = new File(path);
+                    if (imgFile.exists()) {
+                        //Save the path for the current user
+                        inFriend.setImagePath(path);
+                        fd.update(inFriend);
+
+                        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                        ImageView myImage = (ImageView) findViewById(R.id.user_image);
+                        myImage.setImageBitmap(myBitmap);
+                    } else {
+                        Log.d(TAG, "File Not Found!! ");
+                    }
+
+
+                    // Get the file instance
+                    // File file = new File(path);
+                    // Initiate the upload
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
